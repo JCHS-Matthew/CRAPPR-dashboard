4cc6ebf9ca8c288c075a30bdc9aad4989cc1f2cf
Matthew Arck <37804730+JCHS-Matthew@users.noreply.github.com>
2019-09-05 20:08:20 -0500

    separate data into own file

---
+++ data.js
@@ -0,0 +1,18 @@
+let leaderboard_date = "9/5/2019"
+let players = {
+  Andrew: {mean: 16.302, sd: 1.285, change: 2}, 
+  Ben: {mean: 24.229, sd: 0.9, change: 3}, 
+  Chloe: {mean: 10.702, sd: 1.547, change: 1}, 
+  Gustavo: {mean: 24.299, sd: 1.007, change: 0}, 
+  Isabel: {mean: 14.746, sd: 1.037, change: -1}, 
+  Jeff: {mean: 22.566, sd: 1.467, change: 0}, 
+  Jessica: {mean: 15.119, sd: 3.2, change: -1}, 
+  Laury: {mean: 17.684, sd: 1.547, change: 1}, 
+  Lena: {mean: 24.289, sd: 2.501, change: 0}, 
+  Mark: {mean: 24.239, sd: 1.242, change: -3}, 
+  Matt: {mean: 22.529, sd: 0.873, change: 0}, 
+  Neil: {mean: 16.518, sd: 1.36, change: -1}, 
+  Peter: {mean: 23.722, sd: 0.986, change: 0}, 
+  Sherry: {mean: 17.8, sd: 0.972, change: 0}, 
+  Steve: {mean: 16.299, sd: 1.111, change: -1}, 
+}
