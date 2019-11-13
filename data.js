fcdd18e44f4dea6e732b073254e42d6f099d9303
Matthew Arck <37804730+JCHS-Matthew@users.noreply.github.com>
2019-11-13 14:03:05 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,39 +1,20 @@
 let leaderboard_date = "11/13/2019"
 let players = {
 Andrew: {mean: 22.2, sd: 0.842, change: 0}, 
-
 Ben: {mean: 25.742, sd: 0.82, change: -1}, 
-
 Chloe: {mean: 10.204, sd: 1.155, change: 0}, 
-
 Gustavo: {mean: 23.666, sd: 0.84, change: 0}, 
-
 Isabel: {mean: 14.465, sd: 0.915, change: 0}, 
-
 James: {mean: 9.231, sd: 1.696, change: "NR"}, 
-
 Jeff: {mean: 22.317, sd: 1.251, change: 0}, 
-
 Jessica: {mean: 14.973, sd: 1.074, change: 0}, 
-
 Laury: {mean: 16.304, sd: 1.337, change: 0}, 
-
 Lena: {mean: 26.476, sd: 2.354, change: 0}, 
-
 Mark: {mean: 26.004, sd: 0.833, change: 1}, 
-
 Matt: {mean: 22.798, sd: 0.813, change: 0}, 
-
 Mensur: {mean: 17.721, sd: 3.168, change: "NR"}, 
-
 Neil: {mean: 16.486, sd: 1.338, change: 0}, 
-
 Peter: {mean: 23.432, sd: 0.816, change: 0}, 
-
-Ricardo: {mean: 20.686, sd: 4.429, change: 0}, 
-
 Sherry: {mean: 18.217, sd: 0.843, change: 0}, 
-
 Steve: {mean: 16.346, sd: 1.125, change: 0}, 
-
 }
