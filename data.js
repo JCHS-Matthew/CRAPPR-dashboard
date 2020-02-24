efed96fae5906c5a52583b5ad69881e0893d8097
Matthew Arck <37804730+JCHS-Matthew@users.noreply.github.com>
2020-02-24 14:06:28 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
-let leaderboard_date = "2/10/2020"
+let leaderboard_date = "2/21/2020"
 let players = {
 Peter: {mean: 22.656, sd: 0.823, change: -1}, 
 
-Ben: {mean: 26.995, sd: 0.825, change: 0}, 
+Ben: {mean: 26.716, sd: 0.816, change: 0}, 
 
 Steve: {mean: 16.346, sd: 1.125, change: 0}, 
 
-Gustavo: {mean: 24.558, sd: 0.811, change: 0}, 
+Gustavo: {mean: 24.345, sd: 0.821, change: 0}, 
 
-Jeff: {mean: 21.669, sd: 1.153, change: 0}, 
+Jeff: {mean: 20.922, sd: 1.051, change: 0}, 
 
-Mark: {mean: 25.326, sd: 0.823, change: 0}, 
+Mark: {mean: 25.658, sd: 0.812, change: 0}, 
 
-Sherry: {mean: 17.015, sd: 0.829, change: 0}, 
+Sherry: {mean: 16.989, sd: 0.83, change: 0}, 
 
-Matt: {mean: 22.932, sd: 0.827, change: 1}, 
+Matt: {mean: 23.063, sd: 0.823, change: 1}, 
 
 Neil: {mean: 16.486, sd: 1.338, change: 0}, 
 
-Isabel: {mean: 14.242, sd: 0.858, change: -1}, 
+Isabel: {mean: 14.114, sd: 0.86, change: 0}, 
 
-Andrew: {mean: 23.475, sd: 0.817, change: 0}, 
+Andrew: {mean: 24.144, sd: 0.818, change: 0}, 
 
 Chloe: {mean: 10.198, sd: 1.121, change: 0}, 
 
-Laury: {mean: 15.853, sd: 1.265, change: -1}, 
+Laury: {mean: 15.853, sd: 1.265, change: 0}, 
 
-Jessica: {mean: 14.976, sd: 0.955, change: 2}, 
+Jessica: {mean: 14.896, sd: 0.946, change: 0}, 
 
 Lena: {mean: 26.476, sd: 2.354, change: 0}, 
 
@@ -34,6 +34,6 @@ Mensur: {mean: 16.63, sd: 2.377, change: 0},
 
 James: {mean: 9.066, sd: 1.656, change: 0}, 
 
-David: {mean: 10.742, sd: 1.374, change: 0}, 
+David: {mean: 10.682, sd: 1.295, change: 0}, 
 
 }
