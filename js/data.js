6af8a98632eb6a9588b584647bc56d70fcae30ba
Matthew Arck <37804730+JCHS-Matthew@users.noreply.github.com>
2022-11-03 17:26:59 -0500

    update leaderboard data

--- js/data.js
+++ js/data.js
@@ -1,39 +1,20 @@
-let leaderboard_date = "2/21/2020"
+let leaderboard_date = "11/3/2022"
 let players = {
-    Peter: { mean: 22.656, sd: 0.823, change: -1 },
-
-    Ben: { mean: 26.716, sd: 0.816, change: 0 },
-
-    Steve: { mean: 16.346, sd: 1.125, change: 0 },
-
-    Gustavo: { mean: 24.345, sd: 0.821, change: 0 },
-
-    Jeff: { mean: 20.922, sd: 1.051, change: 0 },
-
-    Mark: { mean: 25.658, sd: 0.812, change: 0 },
-
-    Sherry: { mean: 16.989, sd: 0.83, change: 0 },
-
-    Matt: { mean: 23.063, sd: 0.823, change: 1 },
-
-    Neil: { mean: 16.486, sd: 1.338, change: 0 },
-
-    Isabel: { mean: 14.114, sd: 0.86, change: 0 },
-
-    Andrew: { mean: 24.144, sd: 0.818, change: 0 },
-
-    Chloe: { mean: 10.198, sd: 1.121, change: 0 },
-
-    Laury: { mean: 15.853, sd: 1.265, change: 0 },
-
-    Jessica: { mean: 14.896, sd: 0.946, change: 0 },
-
-    Lena: { mean: 26.476, sd: 2.354, change: 0 },
-
-    Mensur: { mean: 16.63, sd: 2.377, change: 0 },
-
-    James: { mean: 9.066, sd: 1.656, change: 0 },
-
-    David: { mean: 10.682, sd: 1.295, change: 0 },
-
+    Andres: { mean: 25.7628, sd: .7722826, change: 0 },
+    Arun: { mean: 20.69855, sd: 1.047874, change: 1 },
+    Ben: { mean: 26.43062, sd: .5739321, change: 0 },
+    David_S: { mean: 21.14104, sd: 1.227786, change: -3 },
+    Jack: { mean: 22.29344, sd: 1.683666, change: -3 },
+    Jeffrey: { mean: 20.34969, sd: .9020563, change: 0 },
+    Karen: { mean: 14.4919, sd: 2.107686, change: 0 },
+    Laury: { mean: 15.90948, sd: 1.234609, change: -1 },
+    Matt: { mean: 24.87766, sd: .572882, change: 0 },
+    Neil: { mean: 20.34025, sd: .895323, change: 1 },
+    Niraj: { mean: 12.90949, sd: 2.078175, change: 0 },
+    Ricardo: { mean: 25.5815, sd: 2.373292, change: 7 },
+    Rowan: { mean: 22.37892, sd: 1.022033, change: 0 },
+    Sherry_J: { mean: 24.49406, sd: 2.09017, change: -1 },
+    Victor: { mean: 18.36557, sd: 1.213377, change: -1 },
+    Yumeng: { mean: 19.56637, sd: 1.922346, change: 1 },
+    Zeeshan: { mean: 18.81486, sd: 1.072152, change: -1 },
 }
